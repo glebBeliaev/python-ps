7df85cc298864d300a0a17512c24f0cd0601a832
glebBeliaev <flysolo.project@gmail.com>
2026-04-12 07:36:44 +0200

    home_work

---
+++ 2-operations/expences.py
@@ -0,0 +1,8 @@
+expence_car = 5020.25
+expence_food = 1200.00
+expence_fun = 1000.00
+
+total_expences = expence_car + expence_food + expence_fun
+average_expences = total_expences / 3
+print("Total expences: " + str(total_expences))
+print("Average expences: " + str(average_expences))
